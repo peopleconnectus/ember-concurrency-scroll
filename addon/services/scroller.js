155a9433df0263b3468e809c46f7ed24b324ada0
Jake Bixby <jake.bixby@classmates.com>
2017-12-05 17:48:25 -0800

    cleaned up scrolling and added container based scroll

--- addon/services/scroller.js
+++ addon/services/scroller.js
@@ -4,33 +4,77 @@ import Easing from 'easing';
 import { task, timeout } from 'ember-concurrency';
 
 const easeTypes = ['linear', 'quadratic', 'cubic', 'quartic', 'quintic', 'sinusoidal', 'sin', 'circular', 'exponential'];
+
 export default Service.extend({
-  scrollToElementId: task(function * (elementId, duration, type) {
-    let start = document.documentElement.scrollTop || document.body.scrollTop;
-    let end = document.getElementById(elementId).offsetTop;
-    yield this.get('scrollToX').perform(start, end, duration, type);
+  scrollToElementId: task(function * (elementId, options = {}) {
+    let start = this.getDocumentTop();
+    let end = document && document.getElementById(elementId).offsetTop;
+    // if we're targeting a container, account for offset to start and end
+    if (options.container) {
+      let container = this.getContainer(options.container);
+      start = container.scrollTop;
+      end = end - container.offsetTop;
+    }
+    yield this.get('scrollToX').perform(start, end, options);
   }),
 
-  scrollToElement: task(function * (element, duration, type) {
-    let start = document.documentElement.scrollTop || document.body.scrollTop;
+  scrollToElement: task(function * (element, options = {}) {
+    let start = this.getDocumentTop();
     let end = element.offsetTop;
-    yield this.get('scrollToX').perform(start, end, duration, type);
+    // if we're targeting a container, account for offset to start and end
+    if (options.container) {
+      let container = this.getContainer(options.container);
+      start = container.scrollTop;
+      end = end - container.offsetTop;
+    }
+    yield this.get('scrollToX').perform(start, end, options);
   }),
 
   // start position, end position, duration in ms, easetype
-  scrollToX: task(function * (start, end, duration, type) {
-    let easeType = type || 'sinusoidal';
-    assert(`"${type}" is not a valid easeType. It must be one of these options: ${easeTypes}`, easeTypes.indexOf(easeType) != -1);
+  scrollToX: task(function * (start, end, options = {}) {
+    console.log('scrollto ', start, end)
+    let easeType = options.type || 'sinusoidal';
+    let duration = options.duration || 1000;
+    let container = options.container && this.getContainer(options.container) || window;
+    let scrollTo = this.getScrollTo(container);
+    assert(`"${options.type}" is not a valid easeType. It must be one of these options: ${easeTypes}`, easeTypes.indexOf(easeType) !== -1);
     let index = 0,
       delay = duration * 0.001,
       steps = Math.ceil(duration * 0.1),
       target = end - start,
       eases = Easing(steps, easeType);
     while (index < steps) {
-      window.scrollTo(0, ( eases[index] * target ) + start);
+      scrollTo(0, ( eases[index] * target ) + start);
       index++;
       yield timeout(delay);
     }
+  }).keepLatest(),
+
+  cancelAll() {
     this.get('scrollToX').cancelAll();
-  }).drop()
+  },
+
+  getScrollTo(container) {
+    if (container === window) {
+      return container.scrollTo;
+    } else {
+      return (y, x) => {
+        container.scrollTop = x;
+      };
+    }
+  },
+
+  getContainer(container) {
+    if (typeof container !== 'string') {
+      return container;
+    } else {
+      // assume the string is an element id
+      return document && document.getElementById(container);
+    }
+  },
+
+  getDocumentTop() {
+    assert('document is not available', document);
+    return document && document.documentElement.scrollTop || document && document.body.scrollTop || 0;
+  }
 });
--- tests/dummy/app/components/element-scroll.js
+++ tests/dummy/app/components/element-scroll.js
@@ -1,9 +1,26 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
 export default Component.extend({
   tagName: 'button',
   scroller: service(),
+  scroll: task(function *() {
+    yield this.get('scroller.scrollToElementId').perform(...arguments);
+    if (this.get('bounce')){
+      yield this.get('scroller.scrollToElementId').perform('1');
+    }
+  }),
   click() {
-    this.get('scroller.scrollToElementId').perform(this.get('target'), 1500, 'sin');
+    this.get('scroll').perform(this.get('target'), {
+      duration: 1500,
+      type: 'sin'
+    });
+    let cancel = ()=> {
+      // console.log('cancel!', this.get('scroller.scrollToX.state'));
+      if (this.get('scroller.scrollToX.isRunning')) {
+        this.get('scroller.scrollToElementId').perform('1');
+      }
+    };
+    this.$(document.body).one('click', cancel);
   }
 })
