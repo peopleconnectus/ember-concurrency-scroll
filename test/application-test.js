4c6b85cc8b1957ba272b4addb19303c5ef21c2c8
Jake Bixby <jake.bixby@classmates.com>
2017-12-12 15:36:58 -0800

    add longer timeout to allow for install

--- test/application-test.js
+++ test/application-test.js
@@ -6,7 +6,7 @@ const request = denodeify(require('request'));
 const AddonTestApp = require('ember-cli-addon-tests').AddonTestApp;
 
 describe('Acceptance | Application', function() {
-  this.timeout(300000);
+  this.timeout(500000);
 
   let app;
 
