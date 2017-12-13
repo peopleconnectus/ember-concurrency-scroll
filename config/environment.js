e2169a0c2238895ce3587c339f599d9b3c323b54
Kelly Selden <kellyselden@gmail.com>
2017-12-12 16:33:37 -0800

    reset config

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,6 @@
 /* eslint-env node */
 'use strict';
 
-module.exports = function(environment, appConfig) {
-  appConfig['ember-concurrency-scroll'] = appConfig['ember-concurrency-scroll'] || {};
+module.exports = function(/* environment, appConfig */) {
   return { };
 };
