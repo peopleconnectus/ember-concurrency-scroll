9dc9306b9048c1926743f700a2309c8510ea6217
Kelly Selden <kellyselden@gmail.com>
2017-12-15 11:13:37 -0800

    use pkg-conf instead of manual lookup

--- blueprints/ember-concurrency-scroll/index.js
+++ blueprints/ember-concurrency-scroll/index.js
@@ -1,23 +1,20 @@
 /* eslint-env node */
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
   description: 'defaultBlueprint for ember-concurrency-scroll',
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall() {
-    let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf8'));
-
-    // Add addons to package.json and run defaultBlueprint
-    return this.addAddonsToProject({
-      // a packages array defines the addons to install
-      packages: [
-        // name is the addon name, and target (optional) is the version
-        {name: 'ember-cli-cjs-transform', target: pkg.peerDependencies['ember-cli-cjs-transform']}
-      ]
+    return require('pkg-conf')('peerDependencies').then(peerDependencies => {
+      // Add addons to package.json and run defaultBlueprint
+      return this.addAddonsToProject({
+        // a packages array defines the addons to install
+        packages: [
+          // name is the addon name, and target (optional) is the version
+          {name: 'ember-cli-cjs-transform', target: peerDependencies['ember-cli-cjs-transform']}
+        ]
+      });
     });
   }
 };
