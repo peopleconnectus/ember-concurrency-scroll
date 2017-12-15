0be360268f8f3f2c4cf5413a7654a784da8e8880
Kelly Selden <kellyselden@gmail.com>
2017-12-15 10:24:50 -0800

    tie ember-cli-cjs-transform blueprint version to package.json

--- blueprints/ember-concurrency-scroll/index.js
+++ blueprints/ember-concurrency-scroll/index.js
@@ -1,15 +1,22 @@
 /* eslint-env node */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   description: 'defaultBlueprint for ember-concurrency-scroll',
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall() {
+    let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf8'));
+
     // Add addons to package.json and run defaultBlueprint
     return this.addAddonsToProject({
       // a packages array defines the addons to install
       packages: [
         // name is the addon name, and target (optional) is the version
-        {name: 'ember-cli-cjs-transform'}
+        {name: 'ember-cli-cjs-transform', target: pkg.peerDependencies['ember-cli-cjs-transform']}
       ]
     });
   }
