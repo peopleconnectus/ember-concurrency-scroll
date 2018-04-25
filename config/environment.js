94ce06f530816f062de3c5cf129bf13c28390e03
Kelly Selden <kellyselden@gmail.com>
2018-04-25 11:38:13 -0700

    add overrides

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function(environment, appConfig) {
+  let testingOverrides = {};
+  if (environment === 'test' && !process.env.CONCURRENCY_SCROLL_TEST) {
+    testingOverrides.duration = 1;
+  }
+  return {
+    'ember-concurrency-scroll': {
+      overrides: Object.assign(
+        {},
+        testingOverrides,
+        (appConfig['ember-concurrency-scroll'] || {}).overrides || {}
+      )
+    }
+  };
 };
